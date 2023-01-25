c648d16926e2130f1fd53f8c3b5a5e19343ce9d3
Marcel1-MTL <123545925+Marcel1-MTL@users.noreply.github.com>
2023-01-25 10:39:26 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    led.toggle(0, 0)
+    while (false) {
+        if (true) {
+        	
+        } else {
+        	
+        }
+        radio.sendString("")
+    }
+})
+radio.setGroup(1)
+basic.forever(function () {
+    music.playMelody("- - - - - - - - ", 120)
+})
